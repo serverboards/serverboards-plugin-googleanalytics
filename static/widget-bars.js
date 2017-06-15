2fa450faeafd21042f74a7f5c9355fb67c63b6cd
David Moreno <dmoreno@coralbits.com>
2017-06-15 13:36:48 +0200

    Do not refresh often, max every 150 s

--- static/widget-bars.js
+++ static/widget-bars.js
@@ -1,14 +1,21 @@
 (function(){
   let {BarGraph} = Serverboards.graphs
-  let {plugin, store} = Serverboards
+  let {plugin, store, moment} = Serverboards
   const DATE_FORMAT = "YYYY-MM-DDTHH:mm"
+  const MAX_REFRESH_PERIOD_S = 120
 
   function main(el, config, context){
     let graph=new BarGraph(el)
     let analytics
     graph.set_loading()
 
+    let last_update_timestamp=0
     function update(){
+      let current_update_timestamp=moment().unix()
+      if (current_update_timestamp - last_update_timestamp < MAX_REFRESH_PERIOD_S)
+        return
+      last_update_timestamp=current_update_timestamp
+
       let {start, end} = store.getState().project.daterange
 
 
